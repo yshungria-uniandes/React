f4c549c8f77f7c0b3f07aba7cbbcdd2c2a0d9491
yshungria-uniandes <ys.hungria@uniandes.edu.co>
2024-11-10 15:09:35 -0500

    Function map in React

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,16 +1,27 @@
-import { Fragment } from "react";
-
 function ListGroup() {
+  const items = [
+    "New York",
+    "Los Angeles",
+    "Chicago",
+    "Houston",
+    "Phoenix",
+    "Philadelphia",
+    "San Antonio",
+    "San Diego",
+    "Dallas",
+  ];
+
   return (
-    <Fragment>
+    <>
+      <h1>List Group</h1>
       <ul className="list-group">
-        <li className="list-group-item">An item</li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-        <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">And a fifth one</li>
+        {items.map((item, index) => (
+          <li key={index} className="list-group-item">
+            {item}
+          </li>
+        ))}
       </ul>
-    </Fragment>
+    </>
   );
 }
 
