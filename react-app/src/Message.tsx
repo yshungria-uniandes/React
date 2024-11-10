76388f3190a937a4ee7c5298a3fcbb4504b09f5f
yshungria-uniandes <ys.hungria@uniandes.edu.co>
2024-11-10 14:20:14 -0500

    React with Mosh

---
+++ react-app/src/Message.tsx
@@ -0,0 +1,13 @@
+// Component Name: Message
+// PascalCasings
+
+function Message() {
+    // JSX: JavaScript XML
+    const name = "Yojan";
+    if(name){
+        return <h1>Hello, {name}</h1>;
+    }
+    return <h1>Hello, Stranger</h1>;
+}
+
+export default Message;
